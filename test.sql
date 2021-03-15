e3c373f9f349991a921771b5c5f0e2dd19a3002a
xiaqing <xiaqing@baiyaodajiankang.com>
2021-03-15 09:03:28 +0800

    sql脚本

---
+++ test.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS `test`;
+CREATE DATABASE `test`;
+use `test`;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `tel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES (11, 'tom', '123456', 'tom@qq.com', '13685249632');
+INSERT INTO `t_user` VALUES (13, 'jack', '123456', 'jack@126.com', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
