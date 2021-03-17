2722cccbe95a2345f5a678a634a41f43849b12bb
xiaqing <xiaqing@baiyaodajiankang.com>
2021-03-17 13:52:47 +0800

    文件上传

--- src/main/java/com/example/controller/FileController.java
+++ src/main/java/com/example/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import com.example.common.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.util.Comparator;
 import java.util.List;
 
@@ -34,6 +39,7 @@ public class FileController {
             try {
                 FileUtil.writeBytes(file.getBytes(), filePath + fileName);
                 System.out.println(fileName + "--上传成功");
+
             } catch (IOException e) {
                 System.err.println(fileName + "--文件上传失败");
             }
@@ -48,22 +54,38 @@ public class FileController {
      * @return
      */
     @PostMapping("/avatar")
-    public Result<?> avatarUpload(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
+    public Result<String> avatarUpload(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
         String baseFilePath = System.getProperty("user.dir") + "/src/main/resources/static/file/";
-        String filePath = baseFilePath + "avatar-" + System.currentTimeMillis() + "." + FileUtil.extName(fileName);
+        String fileName = "avatar-" + System.currentTimeMillis();
+        String filePath = baseFilePath + "avatar-" + System.currentTimeMillis() + "." + FileUtil.extName(originalFilename);
         try {
-            FileUtil.writeBytes(file.getBytes(), filePath);
+            file.transferTo(new File(filePath));
             System.out.println("头像上传成功");
         } catch (IOException e) {
             System.err.println("头像上传失败");
         }
-        return Result.success();
+        return Result.success(fileName);
     }
 
     @GetMapping("/avatar")
-    public Result<String> avatarPath() {
-        List<String> fileNames = FileUtil.listFileNames(System.getProperty("user.dir") + "/src/main/resources/static/file/");
-        return Result.success("file/" + fileNames.stream().filter(name -> name.contains("avatar")).max(Comparator.naturalOrder()).orElse(""));
+    public void avatarPath(HttpServletResponse response) {
+        OutputStream os;
+        String basePath = System.getProperty("user.dir") + "/src/main/resources/static/file/";
+        List<String> fileNames = FileUtil.listFileNames(basePath);
+        String avatar = fileNames.stream().filter(name -> name.contains("avatar")).max(Comparator.naturalOrder()).orElse("");
+        try {
+            if (StrUtil.isNotEmpty(avatar)) {
+                response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(avatar, "UTF-8"));
+                response.setContentType("application/octet-stream");
+                byte[] bytes = FileUtil.readBytes(basePath + avatar);
+                os = response.getOutputStream();
+                os.write(bytes);
+                os.flush();
+                os.close();
+            }
+        } catch (Exception e) {
+            System.out.println("文件下载失败");
+        }
     }
 }
