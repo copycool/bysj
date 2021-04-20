d68d1c8f81acb97de61055b2d0012e8c4a2b8075
xqnode <xiaqingweb@163.com>
2021-04-20 10:01:30 +0800

    一波广告

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,16 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @MapperScan("com.example.mapper")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.warn("关注微信公众号：Java学习指南，获取更多Java干货教程");
     }
 }
